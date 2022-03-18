2a1946169d30f3d02f5ca2afb96de37b939945f5
Scythe <hpa61@sfu.ca>
2022-03-18 14:25:28 -0700

    Update test_playlist.py
    update test

---
+++ ci/v1.1/test_playlist.py
@@ -0,0 +1,35 @@
+"""
+Test the *_original_artist routines.
+
+These tests are invoked by running `pytest` with the
+appropriate options and environment variables, as
+defined in `conftest.py`.
+"""
+
+# Standard libraries
+
+# Installed packages
+import pytest
+
+# Local modules
+import playlist
+
+
+@pytest.fixture
+def pserv(request, playlist_url, auth):
+    return playlist.Playlist(playlist_url, auth)
+
+
+@pytest.fixture
+def plist(request):
+    # To Do
+    return None
+
+
+def test_create(pserv, plist):
+    # Test creation of playlist
+    plist_id = "plist_1"
+    list_name = "test"
+    play_list = [plist[0], plist[1]]
+    trc, p_id = pserv.create(plist_id, list_name, play_list)
+    assert trc == 200 and p_id == plist_id
