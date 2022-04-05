135b0c5355011de4a456ed8f47b4f57aea89728b
Liu <39852426+Yukhon@users.noreply.github.com>
2022-04-04 22:11:51 -0700

    a little update

--- ci/v1.1/test_playlist.py
+++ ci/v1.1/test_playlist.py
@@ -20,27 +20,17 @@ import music
 def pserv(request, playlist_url, auth):
     return playlist.Playlist(playlist_url, auth)
 
-
-@pytest.fixture
-def mserv(request, music_url, auth):
-    return music.Music(music_url, auth)
-
-
 @pytest.fixture
 def plist(request):
-    # To Do
     return ('MyPlayList')
 
 
-def test_simple_run(pserv, mserv):
-
-    # Test creation of playlist
-    song1 = ('k. d. lang',  'Hallelujah')
-    song2 = ('Kris',  'Bad girl')
-    trc1, m_id1 = mserv.create(song1[0], song1[1])
-    trc2, m_id2 = mserv.create(song2[0], song1[1])
-    # plist_id = "plist_1"
-    list_name = "mylist"
-    play_list = [m_id1, m_id2]
-    trc, p_id = pserv.create(list_name, play_list)
-    assert trc == 200
+def test_update(pserv, mserv):
+    origin_list_name = ' what is love '
+    new_list_name = ' what is IBMer '
+    trc1, p_id = pserv.create(origin_list_name)
+    assert trc1 == 200
+    trc2 = pserv.update_playlist_listname(p_id, new_list_name)
+    assert trc2 == 200
+    trc3, listname, list = pserv.read(p_id)
+    assert trc3 == 200 and listname == new_list_name and list == []
