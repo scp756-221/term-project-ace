3b4e35520166f9b9cda6e07facac0d7a046d46be
jie980 <minjiejess@gmail.com>
2022-04-10 20:35:07 -0700

    Update app.py

--- s3/app.py
+++ s3/app.py
@@ -136,32 +136,28 @@ def create_play_list():
     return (response.json())
 
 
-# @bp.route('/write_music_to_playlist/<playlist_id>', methods=['PUT'])
-# def write_music_to_playlist(playlist_id):
-#     headers = request.headers
-#     # check header here
-#     if 'Authorization' not in headers:
-#         return Response(json.dumps({"error": "missing auth"}),
-#                         status=401,
-#                         mimetype='application/json')
-#     try:
-#         content = request.get_json()
-#         music_add = content['MusicAdd']
+@bp.route('/write_to_list/<playlist_id>', methods=['PUT'])
+def write_music_to_playlist(playlist_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        music_add = content['MusicAdd']
 
-#     except Exception:
-#         return json.dumps({"message": "error reading arguments"})
-#     payload = {"objtype": "playlist", "objkey": playlist_id}
-#     url = db['name'] + '/' + db['endpoint'][3]
-#     response = requests.put(
-#         url,
-#         params=payload,
-#         json={"OrigArtist": orig_artist},
-#         headers={'Authorization': headers['Authorization']})
-#     return (response.json())
-# All database calls will have this prefix.  Prometheus metric
-# calls will not---they will have route '/metrics'.  This is
-# the conventional organization.
-app.register_blueprint(bp, url_prefix='/api/v1/playlist/')
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    payload = {"objtype": "playlist", "objkey": playlist_id}
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.put(
+        url,
+        params=payload,
+        json={"OrigArtist": orig_artist},
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
 
 
 # All database calls will have this prefix.  Prometheus metric
